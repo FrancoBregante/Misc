990d35a60f56872a99e223a67f09a63883e3c092
FrancoBregante <fbregante9@gmail.com>
2020-10-30 15:21:07 -0300

    old projects removed

--- Server/controller/getAllBooks.ts
+++
@@ -1,6 +0,0 @@
-import { httpBody } from "./index.controller.ts"
-
-// GET every Book
-export const getAllBooks = ( { response }: httpBody ) => {
-    response.body = library ;
-}
--- Server/controller/index.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Response, Request } from "https://deno.land/x/oak/mod.ts";
-
-// HTTP Interface
-export interface httpBody {
-  response: Response,
-  request: Request,
-  params: any
-}
